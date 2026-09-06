f085c8baa9b8f0a23af21b458c89d5d1d2aaf7b1
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-06 17:49:55 +0530

    Demo of Input and Output in Array

---
+++ Array(Part-1)/OutputInputElement.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5];
+    int size = sizeof(arr)/sizeof(int);
+    cout<<"Enter 5 Element/Numbers "<<'\n';
+
+    // Input an Element
+    for(int i = 0 ; i<size ; i++){
+        cout<<"Enter Element :"<<'\n';
+        cin>>arr[i];
+    }
+
+    cout<<"Printing an Element "<<'\n';
+    
+    // Output an Element
+    for(int i = 0 ; i<size ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<'\n';
+
+
+    return 0;
+}
