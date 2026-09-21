b0da7d400832e960be3ca2331409cd37a55b6317
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-21 17:08:47 +0530

    AlphaBets Printing

---
+++ Char Array & String/alphaPrint.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    for(char ch = 'A' ; ch<='Z'; ch++){
+        cout<<ch <<" ";
+    }
+
+    cout<<'\n';
+
+    for(char ch = 'a' ; ch<='z'; ch++){
+        cout<<ch <<" ";
+    }
+    cout<<'\n';
+
+    cout<<'A'+1<<'\n';
+}
