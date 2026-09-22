ee8373bb630e32f5c1cefb657e89acb4be96ed40
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-22 19:10:06 +0530

    Demo For Each Loop

---
+++ Char Array & String/forEachLoop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+
+    string str = "hello";
+    // length()
+    // size()
+    // for(int i = 0 ; i<str.length(); i++){
+    //     cout<<str[i]<<"-";
+    // }
+    // cout<<'\n';
+
+    for(char ch : str){
+        cout<<ch<<",";
+    }
+    cout<<'\n';
+
+
+
+    return 0;
+}
