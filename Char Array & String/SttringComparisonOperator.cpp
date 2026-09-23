210a1b63df54e1b0953ebf626c2a79626a225c5d
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-09-23 07:25:54 +0530

    String Comparison

---
+++ Char Array & String/SttringComparisonOperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    string str1 = "cat";
+    string str2 = "dog";
+
+    cout<<boolalpha;
+    cout<<(str1 == str2)<<"\n";
+    cout<<(str1 != str2)<<"\n";
+
+    return 0;
+}
